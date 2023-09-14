8089b85113e80b551182bcf43b205bb974be4725
bharathkumar-reddy <pbharathkumarrr@gmail.com>
2023-09-14 13:18:50 +0530

    Implemented Match all query in springboot API with Elastic Search

---
+++ src/main/java/com/elasticsearch/SpringBootElastisearchExample/Service/ElasticSearchService.java
@@ -0,0 +1,2 @@
+package com.elasticsearch.SpringBootElastisearchExample.Service;public class ElasticSearchService {
+}
---
+++ src/main/java/com/elasticsearch/SpringBootElastisearchExample/Util/ElasticSearchUtil.java
@@ -0,0 +1,2 @@
+package com.elasticsearch.SpringBootElastisearchExample.Utility;public class ElasticSearchUtil {
+}
